b14733a75dbd23bd993ea5bb2230bc3cd989b985
Caleb D. Sim <103973059+CodeLamp168@users.noreply.github.com>
2023-02-05 10:29:48 +0800

    Interactive Card Fixed Code
    Complete update of Code using SCSS and some cleaner code methods.

--- script.js
+++ script.js
@@ -1,173 +1,138 @@
-const input_name = document.getElementById('name')
-const input_card_number = document.getElementById('number')
-const input_month = document.getElementById('expire_month')
-const input_year = document.getElementById('expire_year')
-const input_cvc = document.getElementById('input_cvc')
+const form = document.querySelector('.form-area');
 
+const nameInput = document.querySelector('.card-name-input')
+const cardInput = document.querySelector('.card-number-input')
+const monthInput = document.querySelector('.card-month-input')
+const yearInput = document.querySelector('.card-year-input')
+const cvcInput = document.querySelector('.card-cvc-input')
 
+const nameLabel = document.querySelector('.card-name-label')
+const cardLabel = document.querySelector('.card-number-label')
+const monthYearLabel = document.querySelector('.card-exp-date-label')
+const cvcLabel = document.querySelector('.cvc-label')
 
+const nameDisplay = document.querySelector('.card-name-display');
+const numberDisplay = document.querySelector('.card-number-display');
+const dateDisplay = document.querySelector('.card-date-display');
+const cvcDisplay = document.querySelector('.card-cvc-display');
 
-
-let valid = 0;
-let error = 0;
-
-const form = document.getElementById('js-form')
-const thanks = document.getElementById('js-thanks')
-
-
+const thankYou = document.querySelector('.thank-you-card')
+const finishedBtn = document.querySelector('.finished-btn')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     cardValidation()
-    addHtml()
-    checkValid()
+    cardDisplay()
+  
+});
+  
+
+function cardValidation(){
+  const nameValue = nameInput.value.trim()
+  const cardValue = cardInput.value.trim()
+  const monthValue = monthInput.value.trim()
+  const yearValue = yearInput.value.trim()
+  const cvcValue = cvcInput.value.trim()
+
+  let nameValid = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i
+  let cardValid = /^[0-9]{16}$/
+  let cvcValid = /^[0-9]{3}$/
+  let monthValid = /^((0[1-9])|(1[0-2]))$/
+  let yearValid = /^((0[1-9])|(1[0-9])|(2[0-9]|3[0-9]))$/
+
+  const defaultName = 'John Doe'
+  const defaultCard = '0000 0000 0000 0000'
+  const defaultMonth = '00'
+  const defaultYear = '00'
+  const defaultCvc = '000'
+
+  if(nameValue === '' || (!nameValue.match(nameValid))){
+    nameInput.classList.remove('valid')
+    nameInput.classList.add('error')
+    nameLabel.classList.add('error-label')
+    nameHtml = defaultName
+  }
+  else {
+    nameInput.classList.remove('error')
+    nameInput.classList.add('valid')
+    nameLabel.classList.remove('error-label')
+    nameHtml = nameValue
+  }
+
+  if(cardValue === '' || (!cardValue.match(cardValid))){
+    cardInput.classList.remove('valid')
+    cardInput.classList.add('error')
+    cardLabel.classList.add('error-label')
+
+    cardHtml = defaultCard
+  }
+  else {
+    cardInput.classList.remove('error')
+    cardLabel.classList.remove('error-label')
+    cardInput.classList.add('valid')
+    cardHtml = cardValue.replace(/\d{4}/g, '$& ').trim();
+  }
+
+  if(monthValue === '' || (!monthValue.match(monthValid))){
+    monthInput.classList.remove('valid')
+    monthInput.classList.add('error')
+    monthYearLabel.classList.add('error-label')
+    monthHtml = defaultMonth
+  }
+  else {
+    monthInput.classList.remove('error')
+    monthInput.classList.add('valid')
+  }
+
+  if(yearValue === '' || (!yearValue.match(yearValid))){
+    yearInput.classList.remove('valid')
+    yearInput.classList.add('error')
+    monthYearLabel.classList.add('error-label')
+    yearHtml = defaultYear
+  }
+  else {
+    yearInput.classList.remove('error')
+    yearInput.classList.add('valid')
+  }
+
+  if((monthValue.match(monthValid)) && (yearValue.match(yearValid)))
+  {
+    monthYearLabel.classList.remove('error-label')
+    monthHtml = monthValue
+    yearHtml = yearValue
+  }
+
+  if(cvcValue === '' || (!cvcValue.match(cvcValid))){
+    cvcInput.classList.remove('valid')
+    cvcInput.classList.add('error')
+    cvcLabel.classList.add('error-label')
+    cvcHtml = defaultCvc
+  }
+  else {
+    cvcInput.classList.remove('error')
+    cvcLabel.classList.remove('error-label')
+    cvcInput.classList.add('valid')
     
-    
-}) 
+    cvcHtml = cvcValue
+  }
 
-
-
-
-function cardValidation() {
-
-    const name = input_name.value.trim()
-    const cardNumber = input_card_number.value.trim()
-    const month = input_month.value.trim()
-    const year = input_year.value.trim()
-    const cvc = input_cvc.value.trim()
-
-
-
-    let nameValid = /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/i
-    let cardNumValidation = /^[0-9]{16}$/
-    let cvcValid = /[0-9]{3}$/
-    let monthValid = /^((0[1-9])|(1[0-2]))$/
-    let yearValid = /^((0[1-9])|(1[0-9])|(2[0-9]|3[0-9]))$/
-
-    const errorName = 'John Monroe Doe'
-    const errorNumberCard = '0000000000000000'
-    const errorMonth = '00'
-    const errorYear = '00'
-    const errorCVC = '000'
-
-    if(name === '') {
-        errorSign(input_name)
-        nameHTML = errorName
-    
-    
-    } else if (name.match(nameValid)){
-        nameHTML = name
-        validSign(input_name)
-        valid++
-        
-        
-    }
-    else if (!name.match(nameValid)) {
-        errorSign(input_name)
-        nameHTML = errorName
-       
-        
-    }
-
-    
-    if(cardNumber.match(cardNumValidation)){
-       numberHtml = cardNumber
-       validSign(input_card_number)
-       valid++
-
-        
-    }else {
-        numberHtml = errorNumberCard
-        errorSign(input_card_number)
-       
-   
-        
-    }
-
-    if(month.match(monthValid)){
-        monthHtml = month
-        validSign(input_month)
-        valid++
-
-    
-
-    } else {
-        errorSign(input_month)
-        monthHtml = errorMonth
-        
-   
-       
-    }
-    if(cvc.match(cvcValid)){
-        cvcHtml = cvc
-        validSign(input_cvc)
-        valid++
-
-
-       
-    }else {
-        errorSign(input_cvc)
-        cvcHtml = errorCVC
-        
-    }
-
-    if(year.match(yearValid)){
-        yearHtml = year
-        validSign(input_year)
-        valid++
-
-    }else {
-        errorSign(input_year)
-        yearHtml = errorYear
-    }
-
-
-
-   
-}
-
-function checkValid() {
-        if(valid >= 6 || valid < 5){
-            valid = 0;
-            
-        }
-        else if(valid = 5){
-            passForm()
-            
-        }
-}
-
-function addHtml() {
-    const card_number = document.getElementById('js-card-number')
-    const card_name = document.getElementById('js-card-name')
-    const card_month = document.getElementById('js-card-expire-month')
-    const card_year = document.getElementById('js-card-expire-year')
-    const card_cvc = document.getElementById('js-card-cvc')
-
-    card_name.innerHTML = `${nameHTML}`
-    card_number.innerHTML = `${numberHtml}`
-    card_month.innerHTML = `${monthHtml}`
-    card_year.innerHTML = `${yearHtml}`
-    card_cvc.innerHTML = `${cvcHtml}`
+    if (nameInput.classList.contains("valid") && cardInput.classList.contains("valid") &&
+      monthInput.classList.contains("valid") && yearInput.classList.contains("valid") &&
+      cvcInput.classList.contains("valid")) {
+      thankYou.style.display = 'block'
+  } else {
+    thankYou.style.display = 'none'
+  }
 
 }
 
-function errorSign(input){
-    const formArea = input
-    const errorNotif = document.querySelector("small")
-    errorNotif.classList.add("error-notification")
-    formArea.className = 'error'
+function cardDisplay() {
+  nameDisplay.innerHTML = `${nameHtml}`
+  numberDisplay.innerHTML = `${cardHtml}`
+  dateDisplay.innerHTML = `${monthHtml} / ${yearHtml}`
+  cvcDisplay.innerHTML = `${cvcHtml}`
 }
 
-function validSign(input){
-    const formArea = input
-    const errorNotif = document.querySelector("small")
-    errorNotif.classList.remove("error-notification")
-    formArea.className = 'valid'
-}
-
-function passForm(){
-    form.classList.add("finished-form")
-    thanks.classList.add("thank-active")
-
-}
+finishedBtn.addEventListener("click", () => {
+  location.reload();
+})
